ed5fcbfe1fc717587f72445a23d46f15c864f1b9
Android-JCPP-Servo <astewart1138@gmail.com>
2023-05-18 16:34:59 -0500

    Integrate displaying errors or data

--- net-ninja-react-course/src/Home.js
+++ net-ninja-react-course/src/Home.js
@@ -4,18 +4,31 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-        .then(res => res.json())
+        .then(res => {
+            if(!res.ok) {
+                throw Error('Failed to connect to the server');
+            }
+            return res.json()
+        })
         .then(data => {
             setBlogs(data);
             setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setError(err.message);
+            setIsPending(false);
+            setBlogs(null);
         });
     }, []);
 
     return ( 
         <div className="home">
+            {error && <div className="error">{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
