d4d39f1b674fddfced34bb3d050d575fb3a3f343
Android-JCPP-Servo <astewart1138@gmail.com>
2023-05-18 16:07:50 -0500

    Add JSON file for dynamic data handling

--- net-ninja-react-course/src/Home.js
+++ net-ninja-react-course/src/Home.js
@@ -2,26 +2,7 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            title: "Wielder History",
-            body: "lorem ipsum...",
-            author: "Warner Valsain",
-            id: 1
-        },
-        {
-            title: "Martial Arts History",
-            body: "lorem ipsum...",
-            author: "Emma Sodergren",
-            id: 2
-        },
-        {
-            title: "Texas History",
-            body: "lorem ipsum...",
-            author: "Sidd Shady",
-            id: 3
-        }
-    ]);
+    const [blogs, setBlogs] = useState([]);
 
     const [name, setName] = useState('"Gears"');
 
