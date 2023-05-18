bbf59701bc33969cba2b26dfc92ac64f1f05ce10
Android-JCPP-Servo <astewart1138@gmail.com>
2023-05-18 14:31:39 -0500

    Add buttons with click events

--- net-ninja-react-course/src/Home.js
+++ net-ninja-react-course/src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("Hello there!");
+    }
+    const handleClickAgain = name => {
+        console.log(`Hello ${name}!`);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me!</button>
+            <button onClick={() => handleClickAgain("Hillary")}>Click Me Again!</button>
         </div>
     );
 }
