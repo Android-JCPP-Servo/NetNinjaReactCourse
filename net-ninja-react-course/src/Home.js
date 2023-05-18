3803964f8fecb38750ca2348f93aaa3657ce5144
Android-JCPP-Servo <astewart1138@gmail.com>
2023-05-18 16:47:18 -0500

    Create custom hook for FETCH requests

--- net-ninja-react-course/src/Home.js
+++ net-ninja-react-course/src/Home.js
@@ -1,36 +1,13 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./UseFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            if(!res.ok) {
-                throw Error('Failed to connect to the server');
-            }
-            return res.json()
-        })
-        .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setError(err.message);
-            setIsPending(false);
-            setBlogs(null);
-        });
-    }, []);
-
+    const {data, isPending, error} = useFetch("http://localhost:8000/blogs");
     return ( 
         <div className="home">
             {error && <div className="error">{error}</div>}
             {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+            {data && <BlogList blogs={data} title="All Blogs" />}
         </div>
     );
 }
