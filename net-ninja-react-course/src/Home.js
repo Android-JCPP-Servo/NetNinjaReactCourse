01ea09f4d1fc1caa971cde45a2c7aa3d9840d3ec
Android-JCPP-Servo <astewart1138@gmail.com>
2023-05-18 16:02:54 -0500

    Use name as test for useEffect

--- net-ninja-react-course/src/Home.js
+++ net-ninja-react-course/src/Home.js
@@ -23,10 +23,12 @@ const Home = () => {
         }
     ]);
 
+    const [name, setName] = useState('"Gears"');
+
     useEffect(() => {
         console.log("useEffect ran!");
-        console.log(blogs);
-    });
+        console.log(name);
+    }, [name]);
 
     const handleDelete = id => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -36,6 +38,8 @@ const Home = () => {
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName("Jackson")}>Change Name</button>
+            <p>{name}</p>
         </div>
     );
 }
